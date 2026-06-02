a6ffbf5d53658fdd95599388666137a266c5e261
Jess <jessicadinh05@gmail.com>
2026-06-02 13:24:20 +1000

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Starting the Frequency Analyzer..."
 
 # Running the Python frequency analyzer
-FREQ_RESULT=$(python3 /app/.github/scriptsfrequency.py /app/data.txt)
+FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
 
 # Running update_readme.sh
 bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
